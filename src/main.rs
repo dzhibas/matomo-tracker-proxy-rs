1e164a534c7d05bfae520049747fc6480b28a6ca
dzhibas <nikolajus@gmail.com>
2023-10-15 20:28:11 +0200

    adding tracing

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,22 @@ use axum::routing::get;
 use axum::Router;
 use controllers::health;
 use dotenv::dotenv;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 mod controllers;
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
+
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                "matomo_tracker_proxy_rs=debug,tower_http=debug,axum::rejection=trace".into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
     let app = Router::new()
         .route("/", get(root))
