23b9a55e95f02ce691e2433b4eaa5b5acd156eaa
dzhibas <nikolajus@gmail.com>
2023-11-21 20:12:39 +0100

    fixing map and test

--- src/queue.rs
+++ src/queue.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 
 pub const QUEUE_LOCK_NAME: &str = "QueuedTrackingLock";
 pub const QUEUE_PREFIX: &str = "trackingQueueV1";
-pub const QUEUE_KEY_MAP: &[(&str, u8)] = &[
+pub const QUEUE_KEY_MAP: &[(&str, u8); 16] = &[
     ("0", 0),
     ("1", 1),
     ("2", 2),
@@ -21,17 +21,19 @@ pub const QUEUE_KEY_MAP: &[(&str, u8)] = &[
     ("f", 15),
 ];
 
-pub fn get_queue_id(uniq_param: impl ToString) -> u8 {
+pub fn get_queue_key(uniq_param: impl ToString) -> u8 {
     let first = uniq_param.to_string().as_str()[..1].to_lowercase();
-    let m:HashMap<&str, u8> = HashMap::from(QUEUE_KEY_MAP);
-    *m.get(first.as_str()).or(Some(&0)).unwrap_or(&0)
+    let map = HashMap::from(*QUEUE_KEY_MAP);
+    *map.get(first.as_str()).or(Some(&0)).unwrap_or(&0)
 }
 
 mod tests {
-    use crate::queue::get_queue_id;
+    use crate::queue::get_queue_key;
     #[test]
     fn test_get_queue_id() {
-        let input = "demoemdoe";
-        assert_eq!(0, get_queue_id(input));
+        assert_eq!(10, get_queue_key("ajdshflkajhdsflkja"));
+        assert_eq!(13, get_queue_key("demo"));
+        assert_eq!(2, get_queue_key("231231231231212"));
+        assert_eq!(15, get_queue_key("fdlkjafaksdjhfkjads"));
     }
 }
